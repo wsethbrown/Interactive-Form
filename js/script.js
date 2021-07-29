420bd8197323dd23a2a1c7feb374ca2664f1c8e9
Seth Brown <wsethbrown@gmail.com>
2021-07-29 17:03:07 -0400

    add onLoad function to highlight Name and add hideJobRole function to only show Other text field if dropdown option is Other

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("We're connected")
+
+//Cause the Name input field to be automatically selected so user can begin typing immediately
+window.onload = function() {
+    document.getElementById('name').focus()
+}
+
+//Allow user to input text into #other-job-role field if they select "Other" in Job Role dropdown
+function hideJobRole(){
+    let jobTitle = document.getElementById('title').value
+    let otherJob = document.getElementById('other-job-role')
+    otherJob.style.display = "none"
+    jobTitle.addEventListener("change", () => {
+        if (jobTitle.value === "other") {
+            otherJob.style.display = "block"
+        } else {
+            otherJob.style.display = "none"
+        }
+    })
+}
+hideJobRole()
